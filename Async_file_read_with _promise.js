d4356d89f1031f90b9f6d72cab55ca3a582044f7
eddie-lab <eddiebob007@gmail.com>
2024-01-20 16:27:53 +0100

    Commented the code

--- Async_file_read_with _promise.js
+++ Async_file_read_with _promise.js
@@ -1,19 +1,27 @@
-const {readFile, writeFile} = require('fs')
-const util = require('util')
-const readFilePromise = util.promisify(readFile)
-const writeFilePromise = util.promisify(writeFile)
+const { readFile, writeFile } = require('fs');
+const util = require('util');
+
+// Promisify the readFile and writeFile functions
+const readFilePromise = util.promisify(readFile);
+const writeFilePromise = util.promisify(writeFile);
 
 const start = async () => {
     try {
-        const first = await readFilePromise('./content/first.txt', 'utf8')
-        const second = await readFilePromise('./content/second.txt', 'utf8')
-        console.log(first, second)
-    } catch (error){
-    console.log(error)
-    }
-}
+        // Read the content of the first file
+        const first = await readFilePromise('./content/first.txt', 'utf8');
 
-start()
+        // Read the content of the second file
+        const second = await readFilePromise('./content/second.txt', 'utf8');
+
+        // Log the contents of both files to the console
+        console.log(first, second);
+    } catch (error) {
+        console.log(error);
+    }
+};
+
+// Invoke the start function to begin the asynchronous operations
+start();
 
 
 //const getText = (path)=>{
